9b874b2ab0b4b004b368ec87410bd2c61ebb7e65
burni4 <artemnarchyk@gmail.com>
2022-09-04 21:07:54 +0300

    Beginner - Lost Without a Map

---
+++ CodeWars/JavaScript/8 kyu/Beginner - Lost Without a Map.js
@@ -0,0 +1,8 @@
+// https://www.codewars.com/kata/57f781872e3d8ca2a000007e/train/javascript 
+function maps(x){
+    let y = [];
+      for(let i = 0; i < x.length; i++) {
+        y.push(x[i] * 2);
+      }
+    return y;
+}
