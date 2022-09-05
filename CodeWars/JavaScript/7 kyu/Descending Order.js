327d3dac8f76c8ec1aca5c906e1fedfdb67e9db7
burni4 <artemnarchyk@gmail.com>
2022-09-06 00:37:17 +0300

    upd

--- CodeWars/JavaScript/7 kyu/Descending Order.js
+++ CodeWars/JavaScript/7 kyu/Descending Order.js
@@ -1,5 +1,5 @@
 // https://www.codewars.com/kata/5467e4d82edf8bbf40000155/train/javascript
-function descendingOrder(n){
+function descendingOrder(n){ 
   
     let arrStrN = String(n).split('');
     
