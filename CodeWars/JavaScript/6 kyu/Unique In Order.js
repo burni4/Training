99398c9f281b447230ae27c7ffcbd8efd624ddd1
burni4 <artemnarchyk@gmail.com>
2022-09-06 23:15:57 +0300

    Unique In Order

--- CodeWars/JavaScript/6 kyu/Unique In Order.js
+++ CodeWars/JavaScript/6 kyu/Unique In Order.js
@@ -1,6 +1,5 @@
 // https://www.codewars.com/kata/54e6533c92449cc251001667/train/javascript 
 function uniqueInOrder(iterable){
-  
     let arr = [],
         elem = null;
     
@@ -9,11 +8,7 @@ function uniqueInOrder(iterable){
        elem = iterable[i];
        arr.push(elem);
      }
-    }
-    
-    return arr;
-    
+    }   
+    return arr; 
   }
 
-  // ['A','B','C','D','A','B'])
-console.log(uniqueInOrder('AAAABBBCCDAABBB')); 
