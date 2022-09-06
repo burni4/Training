9c6822866d95c813552130fb56c3df3605e3667c
burni4 <artemnarchyk@gmail.com>
2022-09-06 19:33:32 +0300

    Century From Year

---
+++ CodeWars/JavaScript/8 kyu/Century From Year.js
@@ -0,0 +1,5 @@
+// https://www.codewars.com/kata/5a3fe3dde1ce0e8ed6000097/train/javascript
+function century(year) {
+    return Math.floor(year/100) + (year % 100 > 0 ? 1 : 0);
+  }
+  console.log(century(1705));
