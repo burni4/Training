8bbfbab6a0482f72596c219a72b32b3c1fe2957f
burni4 <artemnarchyk@gmail.com>
2022-09-06 23:33:41 +0300

    Detect Pangram

--- CodeWars/JavaScript/6 kyu/Detect Pangram.js
+++ CodeWars/JavaScript/6 kyu/Detect Pangram.js
@@ -1,2 +1,14 @@
 // https://www.codewars.com/kata/545cedaa9943f7fe7b000048/train/javascript
 
+function isPangram(string){
+  
+    let alphabet = 'abcdefghijklmnopqrstuvwxyz',
+    lcString = string.toLowerCase(); 
+    
+    for(i = 0; i < alphabet.length ; i++){
+      if(lcString.indexOf(alphabet[i]) === -1){
+        return false;
+      }
+    }
+    return true; 
+}
