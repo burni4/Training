178ec5ee813875d090e8f002b2930febc28e9e5b
burni4 <artemnarchyk@gmail.com>
2022-09-11 20:33:41 +0300

    Count IP Addresses

---
+++ CodeWars/JavaScript/5 kyu/Count IP Addresses.js
@@ -0,0 +1,16 @@
+// https://www.codewars.com/kata/526989a41034285187000de4/train/javascript
+
+function ipsBetween(start, end){
+  
+    let arrStart = start.split("."),
+        arrEnd = end.split("."),
+        res = 0;
+    
+    let a = arrEnd[0] - arrStart[0],
+        b = arrEnd[1] - arrStart[1],
+        c = arrEnd[2] - arrStart[2],
+        d = arrEnd[3] - arrStart[3];
+    
+    
+    return (a * 256 * 256 * 256) + (b * 256 * 256) + (c * 256) + d;
+  }
