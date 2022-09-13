7b1bfe5270be740ace08f80b5eebd3c6c90b1186
burni4 <artemnarchyk@gmail.com>
2022-09-13 23:42:14 +0300

    Sum of Intervals

--- CodeWars/JavaScript/4 kyu/Sum of Intervals.js
+++ CodeWars/JavaScript/4 kyu/Sum of Intervals.js
@@ -1,16 +1,18 @@
 // https://www.codewars.com/kata/52b7ed099cdc285c300001cd/train/javascript
-intervals.sort((a, b) => a[0] - b[0]);
-let total = 0;
-let highest = 0;
-
-for (let i = 0; i < intervals.length; i++) {
-  if (highest <= intervals[i][0] || i === 0) {
-    total += intervals[i][1] - intervals[i][0];
-    highest = intervals[i][1];
-  } else if (highest < intervals[i][1] && highest > intervals[i][0]) {
-    total += intervals[i][1] - highest;
-    highest = intervals[i][1];
+function sumIntervals(intervals) {
+    intervals.sort((a, b) => a[0] - b[0]);
+    let total = 0;
+    let highest = 0;
+  
+  for (let i = 0; i < intervals.length; i++) {
+    if (highest <= intervals[i][0] || i === 0) {
+      total += intervals[i][1] - intervals[i][0];
+      highest = intervals[i][1];
+    } else if (highest < intervals[i][1] && highest > intervals[i][0]) {
+      total += intervals[i][1] - highest;
+      highest = intervals[i][1];
+    }
   }
-}
-
-return total;
+  
+  return total;
+  }
