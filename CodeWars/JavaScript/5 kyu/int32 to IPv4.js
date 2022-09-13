15857b24c555cb342f0454ae66e9eef44d6958b5
burni4 <artemnarchyk@gmail.com>
2022-09-13 13:22:32 +0300

    int32 to IPv4

---
+++ CodeWars/JavaScript/5 kyu/int32 to IPv4.js
@@ -0,0 +1,15 @@
+// https://www.codewars.com/kata/52e88b39ffb6ac53a400022e/train/javascript
+function int32ToIp(int32){
+  
+    let a = 0, b = 0, c = 0, d = 0;
+    
+    a = Math.trunc(int32 / 256 ** 3);
+    
+    b = Math.trunc((int32 % 256 ** 3) / 256 ** 2);
+    
+    c = Math.trunc((int32 % 256 ** 3) % (256 ** 2) / 256);
+    
+    d = Math.trunc((int32 % 256 ** 3) % (256 ** 2) % 256);
+    
+    return `${a}.${b}.${c}.${d}`
+  }
