2ed15b469341fe00077abb7705f797bc502c34a3
burni4 <artemnarchyk@gmail.com>
2022-09-22 11:52:53 +0300

    Fake Binary

--- CodeWars/SQL/8 kyu/Fake Binary.sql
+++ CodeWars/SQL/8 kyu/Fake Binary.sql
@@ -1,3 +1,6 @@
 -- https://www.codewars.com/kata/57eae65a4321032ce000002d/train/sql
 -- # write your SQL statement here: you are given a table 'fakebin' with column 'x', return a table with column 'x' and your result in a column named 'res'.
 SELECT x, REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(x,'1','0'),'2','0'),'3','0'),'4','0'),'5','1'),'6','1'),'7','1'),'8','1'),'9','1') AS res FROM fakebin
+
+-- v2
+SELECT x, TRANSLATE(x, '123456789', '000011111') AS res FROM fakebin
