9c4f172554217a760ef3a37e77e8bb751077308d
burni4 <artemnarchyk@gmail.com>
2022-09-24 16:53:10 +0300

    Sum of odd numbers

---
+++ CodeWars/SQL/7 kyu/Sum of odd numbers.sql
@@ -0,0 +1,6 @@
+/*
+https://www.codewars.com/kata/55fd2d567d94ac3bc9000064/train/sql
+the table "nums" contains an integer "n", the number
+return your result in a column "res"
+*/
+SELECT n * n * n AS res FROM nums 
