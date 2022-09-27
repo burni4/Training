c0b66d37f957ddd6f1b2fbb79cef7c990c4709e9
burni4 <artemnarchyk@gmail.com>
2022-09-27 10:58:29 +0300

    Your order, please

--- CodeWars/JavaScript/6 kyu/Your order, please.js
+++ CodeWars/JavaScript/6 kyu/Your order, please.js
@@ -1,15 +1,15 @@
 // https://www.codewars.com/kata/55c45be3b2079eccff00010f/train/javascript
 function order(words) {
     let arr = words.split(" "),
-        outpuStr = '';
+        outputStr = '';
 
     for (i = 1; i <= 9; i++) {
 
         arr.forEach((elem) => {
             if (elem.indexOf(String(i)) >= 0) {
-                outpuStr += (elem + ' ')
+                outputStr += (elem + ' ')
             }
         });
     }
-    return outpuStr.trimEnd();
+    return outputStr.trimEnd();
 }
