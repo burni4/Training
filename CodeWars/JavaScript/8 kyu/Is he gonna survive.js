5b5f66460a68284c8343688965dcc3923c564d53
burni4 <artemnarchyk@gmail.com>
2022-10-01 12:53:28 +0300

    Opposites Attract

--- CodeWars/JavaScript/8 kyu/Is he gonna survive.js
+++ CodeWars/JavaScript/8 kyu/Is he gonna survive.js
@@ -1,4 +1,4 @@
 // https://www.codewars.com/kata/59ca8246d751df55cc00014c/train/javascript
 function hero(bullets, dragons) {
-    return dragons * 2 <= bullets ? true : false
+    return bullets >= dragons * 2
 }
