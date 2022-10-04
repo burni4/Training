86d7e0324ff9934d5829f2b1289fcacf33ca2bc6
burni4 <artemnarchyk@gmail.com>
2022-10-04 20:28:45 +0300

    SQL with Pokemon Damage Multipliers

--- CodeWars/SQL/7 kyu/SQL with Pokemon Damage Multipliers.sql
+++ CodeWars/SQL/7 kyu/SQL with Pokemon Damage Multipliers.sql
@@ -1,8 +1,8 @@
 -- https://www.codewars.com/kata/5ab828bcedbcfc65ea000099/train/sql
 SELECT 
-      pokemon_name AS pokemon_name, 
-      str * multiplier AS modifiedStrength, 
-      element
+      pokemon.pokemon_name AS pokemon_name, 
+      pokemon.str * multipliers.multiplier AS modifiedStrength, 
+      multipliers.element
 FROM pokemon AS pokemon 
   INNER JOIN multipliers AS multipliers 
       ON pokemon.element_id = multipliers.id
